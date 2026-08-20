b8188a3e253850aae1977b42970ebb07443a2c6c
deonguyen <nvdeo85@gmail.com>
2026-08-20 09:43:12 -0400

    update

--- dev/s3/sq-spec-portal-backend-oidc/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "sq-spec-portal-tfstate"
-    key     = "dev/s3/sq-spec-portal-backend-oidc/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
--- dev/sqs/sq-spec-portal-backend-webhook-sqs/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "sq-spec-portal-tfstate"
-    key     = "dev/sqs/sq-spec-portal-backend-webhook-sqs/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
