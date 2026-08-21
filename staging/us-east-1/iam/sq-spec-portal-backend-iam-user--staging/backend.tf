c244f0c0e5f7d64a9c98c75b2573a130ff5da45a
deonguyen <nvdeo85@gmail.com>
2026-08-20 23:22:11 -0400

    update

--- staging/us-east-1/iam/sq-spec-portal-backend-iam-user--staging/backend.tf
+++ staging/us-east-1/iam/sq-spec-portal-backend-iam-user--staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "sq-spec-portal-tfstate"
-    key     = "staging/iam/sq-spec-portal-backend-iam-user-staging/terraform.tfstate"
+    key     = "staging/us-east-1/iam/sq-spec-portal-backend-iam-user-staging/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
